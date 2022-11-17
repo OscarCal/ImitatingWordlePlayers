e7b95e2dd5636053fb68cd7ac6aa5320396249aa
Oscar C <oscarcal@ucm.es>
2022-11-17 22:38:18 +0100

    First batches created

---
+++ analisis/get_raw_tweets.py
@@ -0,0 +1,91 @@
+import snscrape.modules.twitter as sntwitter
+import pandas as pd
+from pathlib import Path
+from datetime import date, timedelta
+import multiprocessing as mult
+
+maxtweets_per_trial = 5000
+
+def batch1():
+    ini_date = date(2022,1,1)
+    for game in range(196, 277):
+        tweets_list2 = []
+        for trials in range(1,7):
+            for i,tweet in enumerate(sntwitter.TwitterSearchScraper('"Wordle '+str(game)+' '+str(trials)+'/6"'
+                                                                    +' lang:en since:'+str(ini_date)
+                                                                    +' until:'+ str(ini_date + timedelta(days=1))).get_items()):
+                if i>maxtweets_per_trial:
+                    break
+                tweets_list2.append([game , trials ,tweet.date, tweet.id, tweet.content, tweet.user.username, tweet.url])
+        for i,tweet in enumerate(sntwitter.TwitterSearchScraper('"Wordle '+str(game)+' X/6"'
+                                                                    +' lang:en since:'+str(ini_date)
+                                                                    +' until:'+ str(ini_date + timedelta(days=1))).get_items()):
+                if i>maxtweets_per_trial:
+                    break
+                tweets_list2.append([game , 'X' ,tweet.date, tweet.id, tweet.content, tweet.user.username, tweet.url])
+        ini_date = ini_date + timedelta(days=1)
+        tweets_df2 = pd.DataFrame(tweets_list2, columns=['Game', 'Trial' ,'Datetime', 'Tweet Id', 'Text', 'Username', 'URL'])
+        tweets_df2.to_csv('../scraping_raw/' + str(game) + '.csv')
+
+def batch2():
+    ini_date = date(2022,3,23)
+    for game in range(277, 358):
+        tweets_list2 = []
+        for trials in range(1,7):
+            for i,tweet in enumerate(sntwitter.TwitterSearchScraper('"Wordle '+str(game)+' '+str(trials)+'/6"'
+                                                                    +' lang:en since:'+str(ini_date)
+                                                                    +' until:'+ str(ini_date + timedelta(days=1))).get_items()):
+                if i>maxtweets_per_trial:
+                    break
+                tweets_list2.append([game , trials ,tweet.date, tweet.id, tweet.content, tweet.user.username, tweet.url])
+        for i,tweet in enumerate(sntwitter.TwitterSearchScraper('"Wordle '+str(game)+' X/6"'
+                                                                    +' lang:en since:'+str(ini_date)
+                                                                    +' until:'+ str(ini_date + timedelta(days=1))).get_items()):
+                if i>maxtweets_per_trial:
+                    break
+                tweets_list2.append([game , 'X' ,tweet.date, tweet.id, tweet.content, tweet.user.username, tweet.url])
+        ini_date = ini_date + timedelta(days=1)
+        tweets_df2 = pd.DataFrame(tweets_list2, columns=['Game', 'Trial' ,'Datetime', 'Tweet Id', 'Text', 'Username', 'URL'])
+        tweets_df2.to_csv('../scraping_raw/' + str(game) + '.csv')
+
+def batch3():
+    ini_date = date(2022,6,12)
+    for game in range(358, 439):
+        tweets_list2 = []
+        for trials in range(1,7):
+            for i,tweet in enumerate(sntwitter.TwitterSearchScraper('"Wordle '+str(game)+' '+str(trials)+'/6"'
+                                                                    +' lang:en since:'+str(ini_date)
+                                                                    +' until:'+ str(ini_date + timedelta(days=1))).get_items()):
+                if i>maxtweets_per_trial:
+                    break
+                tweets_list2.append([game , trials ,tweet.date, tweet.id, tweet.content, tweet.user.username, tweet.url])
+        for i,tweet in enumerate(sntwitter.TwitterSearchScraper('"Wordle '+str(game)+' X/6"'
+                                                                    +' lang:en since:'+str(ini_date)
+                                                                    +' until:'+ str(ini_date + timedelta(days=1))).get_items()):
+                if i>maxtweets_per_trial:
+                    break
+                tweets_list2.append([game , 'X' ,tweet.date, tweet.id, tweet.content, tweet.user.username, tweet.url])
+        ini_date = ini_date + timedelta(days=1)
+        tweets_df2 = pd.DataFrame(tweets_list2, columns=['Game', 'Trial' ,'Datetime', 'Tweet Id', 'Text', 'Username', 'URL'])
+        tweets_df2.to_csv('../scraping_raw/' + str(game) + '.csv')
+        
+def batch4():
+    ini_date = date(2022,9,1)
+    for game in range(439, 516):
+        tweets_list2 = []
+        for trials in range(1,7):
+            for i,tweet in enumerate(sntwitter.TwitterSearchScraper('"Wordle '+str(game)+' '+str(trials)+'/6"'
+                                                                    +' lang:en since:'+str(ini_date)
+                                                                    +' until:'+ str(ini_date + timedelta(days=1))).get_items()):
+                if i>maxtweets_per_trial:
+                    break
+                tweets_list2.append([game , trials ,tweet.date, tweet.id, tweet.content, tweet.user.username, tweet.url])
+        for i,tweet in enumerate(sntwitter.TwitterSearchScraper('"Wordle '+str(game)+' X/6"'
+                                                                    +' lang:en since:'+str(ini_date)
+                                                                    +' until:'+ str(ini_date + timedelta(days=1))).get_items()):
+                if i>maxtweets_per_trial:
+                    break
+                tweets_list2.append([game , 'X' ,tweet.date, tweet.id, tweet.content, tweet.user.username, tweet.url])
+        ini_date = ini_date + timedelta(days=1)
+        tweets_df2 = pd.DataFrame(tweets_list2, columns=['Game', 'Trial' ,'Datetime', 'Tweet Id', 'Text', 'Username', 'URL'])
+        tweets_df2.to_csv('../scraping_raw/' + str(game) + '.csv')

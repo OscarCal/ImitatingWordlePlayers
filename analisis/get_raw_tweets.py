d8613f021c998ab00cb46540c665762b63407e32
Oscar C <oscarcal@ucm.es>
2022-11-18 23:14:03 +0100

    Second batch

--- analisis/get_raw_tweets.py
+++ analisis/get_raw_tweets.py
@@ -7,8 +7,8 @@ import multiprocessing as mult
 maxtweets_per_trial = 5000
 
 def batch1():
-    ini_date = date(2022,1,1)
-    for game in range(196, 277):
+    ini_date = date(2022,1,27)
+    for game in range(222, 277):
         tweets_list2 = []
         for trials in range(1,7):
             for i,tweet in enumerate(sntwitter.TwitterSearchScraper('"Wordle '+str(game)+' '+str(trials)+'/6"'
@@ -28,8 +28,8 @@ def batch1():
         tweets_df2.to_csv('../scraping_raw/' + str(game) + '.csv')
 
 def batch2():
-    ini_date = date(2022,3,23)
-    for game in range(277, 358):
+    ini_date = date(2022,4,17)
+    for game in range(302, 358):
         tweets_list2 = []
         for trials in range(1,7):
             for i,tweet in enumerate(sntwitter.TwitterSearchScraper('"Wordle '+str(game)+' '+str(trials)+'/6"'
@@ -49,8 +49,8 @@ def batch2():
         tweets_df2.to_csv('../scraping_raw/' + str(game) + '.csv')
 
 def batch3():
-    ini_date = date(2022,6,12)
-    for game in range(358, 439):
+    ini_date = date(2022,7,14)
+    for game in range(390, 439):
         tweets_list2 = []
         for trials in range(1,7):
             for i,tweet in enumerate(sntwitter.TwitterSearchScraper('"Wordle '+str(game)+' '+str(trials)+'/6"'
@@ -70,8 +70,8 @@ def batch3():
         tweets_df2.to_csv('../scraping_raw/' + str(game) + '.csv')
         
 def batch4():
-    ini_date = date(2022,9,1)
-    for game in range(439, 516):
+    ini_date = date(2022,10,7)
+    for game in range(475, 516):
         tweets_list2 = []
         for trials in range(1,7):
             for i,tweet in enumerate(sntwitter.TwitterSearchScraper('"Wordle '+str(game)+' '+str(trials)+'/6"'

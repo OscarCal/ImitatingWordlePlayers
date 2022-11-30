73e49ca0d87423ecd454ee6cc707aef0cf213362
Oscar C <oscarcal@ucm.es>
2022-11-30 12:10:47 +0100

    First tweet processing

--- analisis/get_raw_tweets.py
+++ analisis/get_raw_tweets.py
@@ -7,8 +7,8 @@ import multiprocessing as mult
 maxtweets_per_trial = 5000
 
 def batch1():
-    ini_date = date(2022,3,9)
-    for game in range(263, 277):
+    ini_date = date(2022,3,15)
+    for game in range(269, 272):
         tweets_list2 = []
         for trials in range(1,7):
             for i,tweet in enumerate(sntwitter.TwitterSearchScraper('"Wordle '+str(game)+' '+str(trials)+'/6"'
@@ -28,8 +28,8 @@ def batch1():
         tweets_df2.to_csv('../scraping_raw/' + str(game) + '.csv')
 
 def batch2():
-    ini_date = date(2022,6,9)
-    for game in range(355, 358):
+    ini_date = date(2022,3,18)
+    for game in range(272, 275):
         tweets_list2 = []
         for trials in range(1,7):
             for i,tweet in enumerate(sntwitter.TwitterSearchScraper('"Wordle '+str(game)+' '+str(trials)+'/6"'
@@ -49,8 +49,8 @@ def batch2():
         tweets_df2.to_csv('../scraping_raw/' + str(game) + '.csv')
 
 def batch3():
-    ini_date = date(2022,7,14)
-    for game in range(390, 439):
+    ini_date = date(2022,3,21)
+    for game in range(275, 277):
         tweets_list2 = []
         for trials in range(1,7):
             for i,tweet in enumerate(sntwitter.TwitterSearchScraper('"Wordle '+str(game)+' '+str(trials)+'/6"'
